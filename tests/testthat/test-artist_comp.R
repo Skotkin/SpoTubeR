89ff29597d4b23e95f63bd84b4fafffc1c6324d4
Skotkin <skotkin@smith.edu>
2026-04-15 19:54:49 -0400

    Finished first test file and vignette skeleton

--- tests/testthat/test-artist_comp.R
+++ tests/testthat/test-artist_comp.R
@@ -6,3 +6,36 @@
 # checking if the function prints two messages when provided with a valid input
 # checking that the function returns a list
 # checking that the function still works for artists with less than 10 top songs
+
+URL_spot <- 'https://open.spotify.com/track/65DbTqJKhbwqYbZ1Okr0rc?si=4e9a52796fff470f'
+URL_not <- 'https://www.youtube.com/watch?v=nUsrYVxrDwI&list=RDnUsrYVxrDwI'
+
+testthat::test_that("URL spotify specific works", {
+  # User inputs a non-spotify URL
+  testthat::expect_error(artist_comp('https://www.youtube.com/watch?v=nUsrYVxrDwI&list=RDnUsrYVxrDwI'))
+  })
+
+testthat::test_that("URL for artist works", {
+  # User inputs a URL to a song instead of an artist
+  testthat::expect_error(artist_comp('https://open.spotify.com/track/65DbTqJKhbwqYbZ1Okr0rc?si=4e9a52796fff470f'))
+  })
+
+testthat::test_that("type is a character", {
+  # User inputs a non-character type
+  testthat::expect_error(artist_comp(12))
+})
+
+testthat::test_that("two messages works", {
+  # checking if the function prints two messages when provided with a valid input
+  testthat::expect_message(artist_comp('https://open.spotify.com/artist/6BRxQ8cD3eqnrVj6WKDok8?si=ImCQ7g50TBaQQ2ikChEZdg'))
+})
+
+testthat::test_that("list return works", {
+  # checking that the function returns a list
+  testthat::expect_identical(class(artist_comp('https://open.spotify.com/artist/7zPS0577bqv2gQDSDa34Gf?si=844m5H6FRYSatUwqbnzSaA'), "list"))
+})
+
+testthat::test_that("less common artists works", {
+  # checking that the function still works for artists with less than 10 top songs
+  testthat::expect_no_error(artist_comp('https://open.spotify.com/artist/7zPS0577bqv2gQDSDa34Gf?si=844m5H6FRYSatUwqbnzSaA'))
+})
