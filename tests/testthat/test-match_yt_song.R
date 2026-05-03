a1ec6f838763833a7c1b564646becac61217e07e
Skotkin <skotkin@smith.edu>
2026-05-02 21:36:41 -0400

    Finished the new test files

--- tests/testthat/test-match_yt_song.R
+++ tests/testthat/test-match_yt_song.R
@@ -1,1 +1,22 @@
-# tests will be planned and created after the match_yt_song function is finalized
+# checking that the appropriate 2 messages are printed when a Spotify video match is found
+testthat::test_that("match messages work", {
+  testthat::expect_message(object = match_yt_song('https://www.youtube.com/watch?v=E1mU6h4Xdxc'), regexp = "Returning statistics for Spotify Songs corresponding to.*")
+  testthat::expect_message(object = match_yt_song('https://www.youtube.com/watch?v=E1mU6h4Xdxc'), regexp = "Spotify song was identified as.*")
+})
+
+# checking that the output is a list
+testthat::test_that("returns list", {
+  testthat::expect_type(match_yt_song("https://www.youtube.com/watch?v=E1mU6h4Xdxc"), 'list')
+})
+
+# testing what happens if user inputs:
+
+# -a non-YouTube URL
+testthat::test_that("YouTube URL specific", {
+  testthat::expect_error(match_yt_song("https://www.google.com"))
+})
+
+# -a non-character type
+testthat::test_that("character specific", {
+  testthat::expect_error(match_yt_song(8))
+})
---
+++ tests/testthat/test-yt_artist_comp.R
@@ -0,0 +1,34 @@
+# testing what happens if user inputs:
+# -a non-YouTube URL
+# -a URL to a different YouTube page (e.g. to a song instead of an artist)
+# -a non-character type
+# checking if the function prints two messages when provided with a valid input
+# checking that the function returns a list
+# checking that the function still works for artists with less than 10 top songs
+
+testthat::test_that("URL YouTube specific works", {
+  # User inputs a non-YouTube URL
+  testthat::expect_error(yt_artist_comp('https://www.google.com'))
+})
+
+testthat::test_that("type is a character", {
+  # User inputs a non-character type
+  testthat::expect_error(yt_artist_comp(12))
+})
+
+testthat::test_that("two messages works", {
+  # checking if the function prints message output when provided with a valid input
+  testthat::expect_message(object = yt_artist_comp('https://www.youtube.com/watch?v=Ee_uujKuJMI'), regexp = "Retrieving YouTube subscriber count associated with.*")
+  testthat::expect_message(object = yt_artist_comp('https://www.youtube.com/watch?v=Ee_uujKuJMI'), regexp = "Retrieving Spotify statistics associated with.*")
+})
+
+testthat::test_that("list return works", {
+  # checking that the function returns a list
+  testthat::expect_identical(class(yt_artist_comp('https://www.youtube.com/watch?v=Ee_uujKuJMI')), "list")
+})
+
+testthat::test_that("less common artists works", {
+  # checking that the function still works for artists with less than 10 top songs on Spotify and a low YouTube view count (<500)
+  testthat::expect_no_error(yt_artist_comp('https://www.youtube.com/watch?v=Ql2m2VBVgKg'))
+})
+
